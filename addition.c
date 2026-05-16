f5c9f4a03f2299b0e3aa4ea626da3bd47dc7b0ac
manya19ab-create <manya19ab@gmail.com>
2026-05-16 12:19:03 +0530

    Create addition.c

---
+++ addition.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+  printf("Name: MANYA\n");
+  printf("USN: 1JS25CI055\n");
+  printf("Program: Simple Addition of Two Numbers\n");
+
+  int a, b, sum;
+  printf("Enter two numbers:");
+  scanf("%d%d", &a, &b);
+
+  sum=a+b;
+  printf("Sum=%d\n", sum);
+
+  return 0;
+}
